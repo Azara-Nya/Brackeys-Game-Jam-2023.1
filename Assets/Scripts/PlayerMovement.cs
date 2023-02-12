0e7cbe6adae69ee8bde8554ab922e5a6ebd211e1
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2023-02-12 20:17:48 +0000

    Player Movement Finalised

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,68 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed=5f;
-    Vector2 movement;
+    [SerializeField] private float moveSpeed=0.5f;
+    [SerializeField] private float jumpVelocity=5f;
+    [SerializeField] private float jumpRememberTime=0.25f;
+    [SerializeField] private float coyoteTimer;
+    [SerializeField] private float horizontalDamping = 0.5f;
+    [SerializeField] private float horizontalStoppingDaming = 0.75f;
+    [SerializeField] private float horizontalTurningDamping = 0.25f;
+    [SerializeField] private float shortJumpHeight = 0.5f;
+    [SerializeField] private float checkRadius = 0.1f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform grounderObject;
+    private float jumpRemeber;
+    private float coyoteTime;
+    private bool isGrounded;
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+    isGrounded = Physics2D.OverlapCircle(grounderObject.position, checkRadius, whatIsGround);
 
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        coyoteTime -= Time.deltaTime;
+        if(isGrounded)
+        {
+            coyoteTime = coyoteTimer;
+        }
+
+        jumpRemeber -= Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpRemeber = jumpRememberTime;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if(rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * shortJumpHeight);
+            }
+        }
+
+        if(jumpRemeber > 0f && coyoteTime > 0f)
+        {
+            jumpRemeber = 0f;
+            coyoteTime = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+        }
+
+        float horizontalVelocity = rb.velocity.x;
+        horizontalVelocity += Input.GetAxisRaw("Horizontal");
+
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+            {
+                horizontalVelocity *= Mathf.Pow(1f - horizontalStoppingDaming, Time.deltaTime * 10f);
+            }
+        else if(Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(horizontalVelocity))
+            {
+                horizontalVelocity *= Mathf.Pow(1f - horizontalTurningDamping, Time.deltaTime * 10f);
+            }
+        else
+            {
+            horizontalVelocity *= Mathf.Pow(1f - horizontalDamping, Time.deltaTime * 10f);
+            }
+
+        rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
     }
 }
